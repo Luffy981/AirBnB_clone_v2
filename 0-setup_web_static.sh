0fb0afbf31bae1a1b2b9e94700bf8a354dfd069b
luffy981 <jhonsmith981@hotmail.com>
2022-01-12 18:52:12 -0500

    setup web fixed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Bash script that sets up your web servers for the deployment of web_static. It must:
-LOCATION="\location /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
+LOCATION="\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
 DIR="/etc/nginx/sites-available/default"
 sudo apt-get -y update
 sudo apt-get -y upgrade
@@ -10,7 +10,7 @@ echo "Testing HTML" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo useradd ubuntu
 sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i "/server {/ \a\${LOCATION}" $DIR
+sudo sed -i "40i\${LOCATION}" $DIR
 sudo service nginx start
 sudo service nginx restart
 sudo service nginx reload
