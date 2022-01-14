8ce0763438b3be3a21f853f176e77762c3bd721c
luffy981 <jhonsmith981@hotmail.com>
2022-01-13 20:01:48 -0500

    fixed

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ from web_static content folder
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
-env.hosts = ['34.73.133.125', '52.201.252.232']
+env.hosts = ['35.237.96.82', '54.164.136.88']
 
 
 def do_pack():
