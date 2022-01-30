b925119f6e1280b6fa8b30767808a14c9f735364
luffy981 <jhonsmith981@hotmail.com>
2022-01-30 12:27:48 -0500

    python route

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -24,10 +24,11 @@ def redirect_parameter(text):
     return "C {}".format(text.replace("_", " "))
 
 
-@app.route('/python/<text>', strict_slashes=False)
-def python_route(text="is cool"):
+@app.route('/python/<text>', strict_slashes=False, default={'text': "is cool"})
+def python_route(text):
     """Python is cool"""
     return "Python {}".format(text.replace("_", " "))
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
