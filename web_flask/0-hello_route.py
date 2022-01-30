a1a1f0ff91dc6cd84e35aec941731307f66c2ad2
luffy981 <jhonsmith981@hotmail.com>
2022-01-30 10:09:57 -0500

    starting with flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+starts with flask
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    """return a message"""
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
