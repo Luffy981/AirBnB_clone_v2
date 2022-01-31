e38b4482992346fddd8b823908303ce150577654
luffy981 <jhonsmith981@hotmail.com>
2022-01-30 22:16:44 -0500

    states list

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,6 +4,7 @@ starts with flask
 """
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -18,7 +19,7 @@ def teardown(self):
 @app.route('/states_list')
 def states():
     """Display all states"""
-    states = storage.all('State').values()
+    states = storage.all(State).values()
     return render_template('7-states_list.html', states=states)
 
 
