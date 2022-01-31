d576a6831f8dc6f43964dcf9fdb29a0ec42a9fa3
luffy981 <jhonsmith981@hotmail.com>
2022-01-30 21:46:00 -0500

    states list

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+starts with flask
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """Close session"""
+    storage.close()
+
+
+@app.route('/states_list')
+def states():
+    """Display all states"""
+    states = storage.all(State)
+    context = {
+        'states': states
+    }
+    return render_template('7-states_list.html', **context)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
