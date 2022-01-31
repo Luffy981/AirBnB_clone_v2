583cf74e9d2079eda6b23824ba806aacb5cbbee3
luffy981 <jhonsmith981@hotmail.com>
2022-01-30 23:48:49 -0500

    cities by states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,20 +1,25 @@
-<!DOCTYPE html>
-<HTML lang="en">
-    <HEAD>
-        <TITLE>HBNB</TITLE>
-    </HEAD>
-    <BODY>
-        <H1>States</H1>
-	<UL>
-	    {% for value in l_list.values()|sort(attribute='name') %}
-	    <li>{{ value.id }}: <B>{{ value.name }}</B>
-		<UL>
-		    {% for i in value.cities|sort(attribute='name') %}
-		    <li>{{ i.id }}: <B>{{ i.name }}</B></li>
-		    {% endfor %}
-		</UL>
-	    </li>
-	    {% endfor %}
-	</UL>
-    </BODY>
-</HTML>
+#!/usr/bin/python3
+"""Simple Flask app, with additional route"""
+from flask import Flask, abort, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/cities_by_states')
+def run_all_states_and_cities():
+    """All states and cities"""
+    l_list = storage.all(State)
+    return render_template('8-cities_by_states.html', l_list=l_list)
+
+
+@app.teardown_appcontext
+def do_teardown(self):
+    """Closes session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
